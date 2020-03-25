e5dc3e749ccd8f907a3638c307c1b23023a19509
Kevvinn <kevinhu229@gmail.com>
2020-03-25 14:53:24 -0700

    Revert "dev-kevin's version"

--- events/voiceStateScripts/index.js
+++
@@ -1,9 +0,0 @@
-const { userJoin } = require('./userJoin')
-const { userMove } = require('./userMove')
-const { userLeave } = require('./userLeave')
-
-module.exports = {
-    userJoin,
-    userMove,
-    userLeave
-}
