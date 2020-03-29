58a8b8943cfa9fd3781214c0dcf339f4f64056d8
Kevvinn <kevinhu229@gmail.com>
2020-03-28 19:55:35 -0700

    bugfix songe

--- commands/song.js
+++ commands/song.js
@@ -10,7 +10,7 @@ function song(msg) {
         // check if user is the user practicing. 
         msg.reply("(X) You aren't practicing. ")
     } else {
-        whospracticing[msg.member.voice.channel.id + "piece"] = msg.content.replace(BOT_PREFIX + "song", "");
+        whospracticing[msg.member.voice.channel.id + "piece"] = msg.content.replace(BOT_PREFIX + "song ", "");
         //store a start time for each song. 
         msg.reply("Done. ");
     }
