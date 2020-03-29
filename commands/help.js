11434a86679b1fc5907447bdd47cbc8e369644bc
info26 <38963122+info26@users.noreply.github.com>
2020-03-28 22:29:21 -0400

    update to use bot prefix

--- commands/help.js
+++ commands/help.js
@@ -8,35 +8,35 @@ function help(msg) {
         title: 'Shushbot help page',
         description: "Documentation available [here](https://shush-bot.firebaseapp.com/). Report bugs [here](https://forms.gle/A4mA6AYJQFdDm62N9)",
         fields: [{ //kay im gonna start copying the stuff from the website. 
-                name: "$practice",
+                name: BOT_PREFIX + "practice",
                 value: "If your channel has no one practicing, use $practice to start practicing. The bot will announce when it is looking for people to practice."
             },
             {
-                name: "$nomore",
+                name: BOT_PREFIX + "nomore",
                 value: "If you don't want to practice anymore, you can use the $nomore command to tell the bot that you are done practicing."
             },
             {
-                name: "$forcepractice",
+                name: BOT_PREFIX + "forcepractice",
                 value: "This command is mod only. "
             },
             {
-                name: "$forcestop",
+                name: BOT_PREFIX + "forcestop",
                 value: "This command is mod only. "
             },
             {
-                name: "$np",
+                name: BOT_PREFIX + "np",
                 value: "This command will make the bot tell you who is practicing."
             },
             {
-                name: "$song",
+                name: BOT_PREFIX + "song",
                 value: "If you are practicing, this command will set the song you are practicing. Usage: $song <song name>"
             },
             {
-                name: "$excuse",
+                name: BOT_PREFIX + "excuse",
                 value: "Unmutes a user so they can talk / give you feedback. Usage: $excuse <user>"
             },
             {
-                name: "$unexcuse",
+                name: BOT_PREFIX + "unexcuse",
                 value: "Mutes a user once they are done giving you feedback. Usage: $unexcuse <user>"
             }
         ],
