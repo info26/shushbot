08a2064841268317a7426a04c2a72352a0f57f3a
Kevvinn <kevinhu229@gmail.com>
2020-07-01 13:31:07 -0700

    Add catch for configurechannels. Formatting for leaderboard embed.

--- commands/views/boardEmbed.js
+++ commands/views/boardEmbed.js
@@ -4,21 +4,24 @@ const table = require('table');
 const { getNick } = require('../../helpers/getNick')
 
 function boardEmbed(msg, data) {
-
+    
     const Client  = new Discord.Client();
 
-    let possiblePracticed = [['User','Total Practice Time']]
-    for (i = 0; i < data.length; i++){
-        var totalPracTime = secondsToHoursAndMinutes(data[i].info['practiceStats']['totalTime'])
-        var totalPracTime = totalPracTime[1] + "h " + totalPracTime[0] + "m"
-        possiblePracticed.push([getNick(msg.guild.members.cache.find(m => m.id === data[i].userId)), totalPracTime])
-    }
-    console.log(possiblePracticed)
     const embed = new Discord.MessageEmbed()
         .setColor(0xF99806)
         .setTitle('Practice Leaderboard')
-        .addField('Leaderboard', `\`\`\`${table.table(possiblePracticed)}\`\`\``)
 
+    for (i = 0; i < data.length; i++){
+        var totalPracTime = secondsToHoursAndMinutes(data[i].info['practiceStats']['totalTime'])
+        var totalPracTime = totalPracTime[1] + "h " + totalPracTime[0] + "m"
+        if (i == 0){
+            embed.addField(`${i + 1}. ${getNick(msg.guild.members.cache.find(m => m.id === data[i].userId))} ♕`, `**Time Practiced**: ${totalPracTime}\n━━━━━━━━━━━━━━`)
+        } else {
+            embed.addField(`${i + 1}. ${getNick(msg.guild.members.cache.find(m => m.id === data[i].userId))}`, `**Time Practiced**: ${totalPracTime}\n━━━━━━━━━━━━━━`)
+        }
+        
+    }
+    
     msg.channel.send(embed)
 }
 
