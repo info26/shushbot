20f83d0c8e80b3ce2dd2a8f6826bea54dcafaa7d
info26 <38963122+info26@users.noreply.github.com>
2020-07-02 19:35:58 -0400

    Disable functionality

--- events/voiceStateScripts/userJoin.js
+++ events/voiceStateScripts/userJoin.js
@@ -6,17 +6,23 @@ async function userJoin(newMember, oldmember) {
         console.log(newMember.member.id + " unmute because he/she is in a channel which the bot doesn't manage.   " + newMember.channel.id)
     } else {
         //MANAGE THIS USER
-        if (newMember.channel.members.size == 1 && APPLIED_CHANNELS.includes(newMember.channel.id)) {
+        /*if (newMember.channel.members.size == 1 && APPLIED_CHANNELS.includes(newMember.channel.id)) {
             newMember.member.voice.setMute(false, "unmute because he/she is the only user in the channel.")
             whospracticing[newMember.channel.id] = newMember.member.id;
             whospracticing[newMember.channel.id + "piece"] = null;
             //set the starting time. 
             whospracticing[newMember.channel.id + "starttime"] = Date.now();
-        } else {
+        } else {*/
             //member is listening
-            newMember.member.voice.setMute(true, "mute because he/she is listening")
-            console.log(newMember.member.id + " mute because he/she is listening channel size: " + newMember.channel.members.size + "==" + newMember.channel.id)
-        }
+
+
+        //CHANGE, do not make member practice automatically. 
+        newMember.member.voice.setMute(true, "mute because he/she is listening")
+        console.log(newMember.member.id + " mute because he/she is listening channel size: " + newMember.channel.members.size + "==" + newMember.channel.id)
+        
+        
+        
+        //}
     }
 }
 
