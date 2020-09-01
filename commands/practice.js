16b0d8d9b3cf9ea66c1363430a7b0db74e8522d4
PradyRao <pradyraoofficial@gmail.com>
2020-09-01 01:41:10 -0400

    more bug fix

--- commands/practice.js
+++ commands/practice.js
@@ -12,18 +12,19 @@ function practice(msg) {
         // user is in a voice channel.
         practicing = whospracticing[msg.member.voice.channel.id]
 
-        if (practicing == msg.author.id) {
-            msg.reply("(X) You are practicing")
-
-        } else if (practicing.match(/[0-9]{16,}/g)) {
-            msg.reply("(X) The channel already has someone practicing")
-
-        } else if (practicing == "upforgrabs" || practicing == null) {
+        if (practicing == "upforgrabs" || practicing == null) {
             msg.reply("Ok, you're now practicing. ")
             msg.member.voice.setMute(false, "user requested to be practicing. ")
             whospracticing[msg.member.voice.channel.id] = msg.author.id;
             whospracticing[msg.member.voice.channel.id + "piece"] = null;
             whospracticing[msg.member.voice.channel.id + "starttime"] = Date.now();
+        }
+        else if (practicing == msg.author.id) {
+            msg.reply("(X) You are practicing")
+
+        } 
+        else if (practicing.match(/[0-9]{16,}/g)) {
+            msg.reply("(X) The channel already has someone practicing")
         }
     }
 };
