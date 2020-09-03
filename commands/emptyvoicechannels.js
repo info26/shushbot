6c88b1132be1c8048268c05bbae576f6666849aa
PradyRao <pradyraoofficial@gmail.com>
2020-09-02 21:04:54 -0400

    removed perm stuff, its not working, will get back to it in the future

--- commands/emptyvoicechannels.js
+++ commands/emptyvoicechannels.js
@@ -1,10 +1,10 @@
 const { userLeftorNoMore } = require('./../helpers/userLeftorNoMore')
 
 async function disconnectMembers(msg) {
-    var cmd = msg.content.split(" ");
+    //var cmd = msg.content.split(" ");
     //make sure the person writing the command is an admin of the server
     if (msg.member.permissions.has(['ADMINISTRATOR'])) {
-
+        /*
         //find the parent(channel catrgory) of the text channel 
         var chCategory = msg.channel.parent;
         //get the roles that needs to be edited in that channel category
@@ -15,16 +15,17 @@ async function disconnectMembers(msg) {
             roleToBeEdited = await msg.guild.roles.fetch(cmd[1]);
         }
         
-        //get all the voice chats that are in this channel category
-        var vcs = chCategory.children.filter(c => c.type === 'voice');
-
         //change channel category perms so that non-mods can't enter
         chCategory.overwritePermissions([
             {
                 id: roleToBeEdited.id,
                 deny: ['CONNECT', 'SEND_MESSAGES']
             }
-        ]);
+        ]);*/
+
+        //get all the voice chats that are in this channel category
+        var vcs = chCategory.children.filter(c => c.type === 'voice');
+
         //for each voice chat in that category
         vcs.array().forEach(vc => {
             //makes sure the current vc is bot enabled
@@ -44,7 +45,7 @@ async function disconnectMembers(msg) {
                 })
             }
         });
-        msg.reply("Done. Voice Channels are locked and Members are kicked")
+        msg.reply("Done. Members are kicked")
     }
     else {
         msg.reply("You do not have the permissions to execute this command");
